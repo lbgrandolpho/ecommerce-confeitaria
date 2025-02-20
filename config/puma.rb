378adcbf8122cf5794fd396f13c9a51630c2154f
lbgrandolpho <lucas.grandolpho@hotmail.com>
2025-02-20 19:55:25 -0300

    change ports

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-port ENV.fetch("PORT", 4002)
+port ENV.fetch("PORT", 3000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
