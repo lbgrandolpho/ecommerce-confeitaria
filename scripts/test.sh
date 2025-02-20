79bab0b1606ee24b9d5afe64dc32567491a77303
lbgrandolpho <lucas.grandolpho@hotmail.com>
2025-02-20 19:56:27 -0300

    update tests

---
+++ scripts/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+bin/rails db:test:prepare
+rspec
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   it "não permite pedidos com menos de 1 kg" do
-    order = Order.new(total_weight: 0.5)
+    flavor = Flavor.create!(id: 1, name: "Chocolate", price_per_kg: 10)
+    order = Order.new(address: "Some address")
+    order.order_items.new(weight: 0.5, flavor_id: flavor.id)
     expect(order.valid?).to be_falsey
   end
-end
 
-RSpec.describe Order, type: :model do
   it "não permite pedidos sem endereço válido" do
+    flavor = Flavor.create!(id: 1, name: "Chocolate", price_per_kg: 10)
     order = Order.new(address: nil)
+    order.order_items.new(weight: 2.5, flavor_id: flavor.id)
     expect(order.valid?).to be_falsey
+  end
+
+  it "permite pedidos válidos" do
+    flavor = Flavor.create!(id: 1, name: "Chocolate", price_per_kg: 10)
+    order = Order.new(address: "Some address")
+    order.order_items.new(weight: 2.5, flavor_id: flavor.id)
+    expect(order.valid?).to be_truthy
   end
 end
